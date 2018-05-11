c93b4cae854e8d9764523d8a3965b94d55635c9a
Archawin <zepalzth@gmail.com>
2018-05-11 15:58:53 +0700

    Edit board

--- src/snakeandladder/Board.java
+++ src/snakeandladder/Board.java
@@ -7,7 +7,7 @@ import square.SnakeSquare;
 import square.Square;
 
 public class Board {
-	public static final int SIZE = 10;
+	public static final int SIZE = 100;
 	private Square[] squares;
 
 	public Board() {
@@ -15,8 +15,7 @@ public class Board {
 		for (int i = 0; i < squares.length; i++)
 			squares[i] = new Square(i);
 
-		// initSpecialSquare();
-		// squares[4] = new LadderSquare(squares[4].getNumber(), 15);
+		initSpecialSquare();
 		squares[squares.length - 1].setGoal(true);
 	}
 
